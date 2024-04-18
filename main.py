9b3344dc07cf1d91911aa7816e9e03f5fff58849
staviasz <erickesc21@hotmail.com>
2024-04-18 12:09:06 -0300

    reload false in deploy

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ app.include_router(tech_router._router)
 
 if __name__ == "__main__":
     port = PydanticEnv().port
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
